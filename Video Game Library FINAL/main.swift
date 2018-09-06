4fd5ad71593bad05c1d7be268a79fc63d7d029ec
Mason Nesbitt <masonnesbitt@macbook-pro-10.local>
2018-09-06 13:27:45 -0500

    Initial Commit

---
+++ Video Game Library FINAL/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Video Game Library FINAL
+//
+//  Created by Mason Nesbitt on 9/6/18.
+//  Copyright © 2018 Mason Nesbitt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
